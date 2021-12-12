1f984929a0dfff8de1b97725465f64bcba790edc
Chandler <prettyChandler7@gmail.com>
2021-12-12 23:31:02 +0800

    master test

--- src/main/java/com/git/test/GitTest.java
+++ src/main/java/com/git/test/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello world 2");
         System.out.println("hello world 3");
         System.out.println("hello world hot-fix");
+        System.out.println("master test");
     }
 }
